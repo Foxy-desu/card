6db4baf10d8121a0aa633f9bbb5ec3a7057d2177
Alexandra Orliuk <iammeduza@gmail.com>
2024-07-10 15:09:32 +0300

    feat: implement store and card slice along with typed hooks

---
+++ src/store/cards_slice.ts
@@ -0,0 +1,113 @@
+import 'immer';
+import { createSlice, PayloadAction, createAsyncThunk, SerializedError, Dispatch } from "@reduxjs/toolkit";
+import type { RootState } from "./store";
+
+export interface ICard {
+    company: {
+        companyId: string,
+    },
+    customerMarkParameters: {
+        loyaltyLevel: {
+                number: number,
+                name: string,
+                requiredSum: number,
+                markToCash: number,
+                cashToMark: number
+        },
+        mark: number
+    },
+    mobileAppDashboard: {
+        companyName: string,
+        logo: string,
+        backgroundColor: string,
+        mainColor: string,
+        cardBackgroundColor: string,
+        textColor: string,
+        highlightTextColor: string,
+        accentColor: string,
+    },
+};
+export interface ICards {
+    cards: ICard[] | [],
+    isLoading: boolean,
+    error: SerializedError | null,
+};
+const initialState: ICards = {
+    cards: [],
+    isLoading: false,
+    error: null,
+};
+// разобраться с типом any
+export const fetchCards: any = createAsyncThunk (
+    'cards/fetchCards',
+    async (queryOptions: any, {rejectWithValue}) => {
+        const {limit, offset} = queryOptions;
+        try {
+            const options = {
+                method: 'POST',
+                headers: {
+                    'TOKEN': '123',
+                    'Content-Type': "application/json",
+                },
+                body: JSON.stringify({
+                    offset,
+                    limit
+                }),
+            };
+            const response = await fetch('http://devapp.bonusmoney.pro/mobileapp/getAllCompaniesLong', options);
+            const data = await response.json();
+            if (response.status === 200) {
+                return data.companies;
+            } else {
+                if(response.status === 400) {
+                    throw new Error(data.message);
+                };
+                if(response.status === 401) {
+                    throw new Error('Ошибка авторизации');
+                };
+                if(response.status === 500) {
+                    throw new Error('Все упало');
+                };
+            }
+        } catch(error:any) {
+            return rejectWithValue({message: error.message});
+        }
+    }
+)
+export const cardSlice = createSlice({
+    name: 'cards',
+    initialState,
+    reducers: {
+        addCard: (state, action: PayloadAction<ICard>) => {
+            state.cards = [...state.cards, action.payload];
+        },
+        deleteCard: (state, action: PayloadAction<string>) => {
+            state.cards = state.cards.filter(card => card.company.companyId !== action.payload);
+        },
+        addAllCards: (state, action: PayloadAction<ICard[]>) =>{
+            state.cards = [...state.cards,...action.payload];
+        }
+    },
+    extraReducers: (builder) => {
+        builder
+           .addCase(fetchCards.pending, (state) => {
+                state.isLoading = true;
+                state.error = null;
+                state.cards = state.cards;
+            })
+           .addCase(fetchCards.fulfilled, (state, action) => {
+                state.isLoading = false;
+                state.cards = action.payload;
+                state.error = null;
+            })
+           .addCase(fetchCards.rejected, (state, action) => {
+                state.isLoading = false;
+                state.error = action.payload;
+                state.cards = state.cards;
+            });
+    }
+});
+
+export const { addCard, addAllCards, deleteCard} = cardSlice.actions;
+export const selectCards = (state: RootState) => state.cards;
+export default cardSlice.reducer; 
---
+++ src/utils/hooks/hooks.ts
@@ -0,0 +1,6 @@
+import { useDispatch, useSelector } from "react-redux";
+import type { AppDispatch, RootState } from "../../store/store";
+
+//use these hooks instead of default ones
+export const useAppDispatch = useDispatch.withTypes<AppDispatch>();
+export const useAppSelector = useSelector.withTypes<RootState>();
