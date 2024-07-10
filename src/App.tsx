2f77f1b3c60358ffeecaf2c187093c81a02861a4
Alexandra Orliuk <iammeduza@gmail.com>
2024-07-10 15:17:43 +0300

    feat: edit code to test new components

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { animated, useTransition } from '@react-spring/web';
+import SplashScreen from './screens/splash_screen/splash_screen'
+import { useSplash } from './utils/hooks/useSplash';
+import CardsScreen from './screens/cards_screen/cards_screen';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const isAppInited = useSplash();
+  const transition = useTransition(!isAppInited, {
+    from: {opacity: 1},
+    leave: {opacity: 0},
+    config: {duration: 70},
+  })
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {transition((style, item)=> {
+        return item
+        ? <animated.div style={style}><SplashScreen /></animated.div>
+        : <CardsScreen/>
+      })
+      }
     </>
   )
 }
