26651062246e46fc662635afa412ee7712b6daa4
Alexandra Orliuk <iammeduza@gmail.com>
2024-07-10 15:15:30 +0300

    feat:  create card-title component

---
+++ src/components/card_title/card_title.tsx
@@ -0,0 +1,16 @@
+import cl from './card_title.module.scss';
+
+interface ICardTitle {
+    companyName: string;
+    dynamicColor?: string;
+};
+
+const CardTitle = ({companyName, dynamicColor}: ICardTitle): JSX.Element => {
+    const inlineStyle = dynamicColor ? { color: dynamicColor } : {};
+    
+    return (
+        <h2 className={cl.cardTitle} style={inlineStyle}>{companyName}</h2>
+    )
+};
+
+export default CardTitle;
