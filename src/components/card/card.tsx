2041989b10f007bb4cf8b4961a20b9365f02d4b7
Alexandra Orliuk <iammeduza@gmail.com>
2024-07-10 15:14:14 +0300

    feat:  create card component

---
+++ src/components/card/card.tsx
@@ -0,0 +1,91 @@
+import cl from './card.module.scss';
+import IconButton from '../icon_btn/icon_btn';
+import Btn from '../btn/btn';
+import Logo from '../card_logo/card_logo';
+import InfoBlock from '../info_block/info_block';
+import CardTitle from '../card_title/card_title';
+import { ICard } from '../../store/cards_slice';
+
+interface ICardProps {
+    companyId: string;
+    loyaltyLevel: string;
+    cashback: number;
+    credits: number;
+    appDashboard: ICard['mobileAppDashboard']
+}
+const Card =(props: ICardProps)=> {
+    const dynamicColor = props.appDashboard.backgroundColor;
+    const inlineStyle = dynamicColor? {backgroundColor: dynamicColor}: {};
+
+    return(
+        <article className={cl.card} style={inlineStyle}>
+            <header className={cl.cardHeader}>
+                <CardTitle
+                    companyName={props.appDashboard.companyName}
+                    dynamicColor={props.appDashboard.cardBackgroundColor}
+                />
+                <div className={cl.cardIcon}>
+                    <Logo src={props.appDashboard.logo}/>
+                </div>
+            </header>
+            <div className={cl.cardInfo}>
+                <div className={cl.cardScore}>
+                    <InfoBlock
+                        data={props.credits}
+                        title={'баллов'}
+                        type={'spacious'}
+                        dynamicColor={{
+                            title: props.appDashboard.highlightTextColor,
+                            data: props.appDashboard.cardBackgroundColor
+                        }}
+                    />
+                </div>
+                <div className={cl.cardLoyalty}>
+                    <div className={cl.loyaltyWrap}>
+                        <InfoBlock
+                            data={`${props.cashback} %`}
+                            title={'Кешбек'}
+                            type={'narrow'}
+                            dynamicColor={{
+                                title: props.appDashboard.highlightTextColor,
+                                data: props.appDashboard.cardBackgroundColor
+                            }}
+                        />
+                    </div>
+                    <div className={cl.loyaltyWrap}>
+                        <InfoBlock
+                            data={props.loyaltyLevel}
+                            title={'Уровень'}
+                            type={'narrow'}
+                            dynamicColor={{
+                                title: props.appDashboard.highlightTextColor,
+                                data: props.appDashboard.cardBackgroundColor
+                            }}
+                        />
+                    </div>
+                </div>
+            </div>
+            <footer className={cl.cardFooter}>
+                <div className={`${cl.btnWrap} ${cl.btnWrap_sml}`}>
+                    <IconButton
+                        iconBtnType='eye'
+                        dynamicColor={props.appDashboard.textColor}/>
+                </div>
+                <div className={`${cl.btnWrap} ${cl.btnWrap_sml}`}>
+                    <IconButton
+                        iconBtnType='trash'
+                        dynamicColor={props.appDashboard.mainColor}/>
+                </div>
+                <div className={cl.btnWrap}>
+                    <Btn dynamicColor={{
+                        text: props.appDashboard.textColor,
+                        bg: props.appDashboard.accentColor
+                        }}
+                    />
+                </div>
+            </footer>
+        </article>
+    )
+};
+
+export default Card;
