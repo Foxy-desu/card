9b15088db723afc9a2955116fef3b4322ac25581
Alexandra Orliuk <iammeduza@gmail.com>
2024-07-12 22:33:58 +0300

    feat: improve  inline styling

--- src/components/icon_btn/icon_btn.tsx
+++ src/components/icon_btn/icon_btn.tsx
@@ -2,31 +2,27 @@ import { ComponentPropsWithoutRef, useState } from 'react';
 import eyeIcon from '../../assets/icons/eye_white.png';
 import trashIcon from '../../assets/icons/trash_white.png';
 import cl from './icon_btn.module.scss';
-import myColors from '../../styles/colors.module.scss';
-
 
 interface IIconBtnProps extends ComponentPropsWithoutRef<'button'> {
     iconBtnType: 'eye'| 'trash';
     dynamicColor?: string;
-}
-
+};
 
 const IconButton = ({iconBtnType, dynamicColor,...props}:IIconBtnProps): JSX.Element => {
     const [btnType] = useState(()=> getImageSource(iconBtnType));
-    
     function getImageSource(type: string) {
         switch (type) {
             case 'eye':
-                return {src:eyeIcon, desc: 'показать', defaultColor: myColors.bgInform};
+                return {style: {WebkitMaskImage: `url(${eyeIcon})`, backgroundColor: dynamicColor}, desc: 'показать'};
             case 'trash':
-                return {src:trashIcon, desc: 'удалить', defaultColor: myColors.bgCaution};
+                return {style: {WebkitMaskImage: `url(${trashIcon})`, backgroundColor: dynamicColor}, desc: 'удалить'};
             default:
-                return {src:'', desc: ''};
+                return {style: {}, desc: ''};
         }
     };
     return (
         <button className={cl.iconButton} type='button' {...props}>
-            <span className={cl.icon} style={{WebkitMaskImage: `url(${btnType.src})`, backgroundColor: dynamicColor || btnType.defaultColor}}/>
+            <span className={`${cl.icon} ${iconBtnType === 'trash' && cl.icon_delete}`} style={btnType.style}/>
             <span className={cl.visuallyHidden}>{btnType.desc}</span>
         </button>
     )
