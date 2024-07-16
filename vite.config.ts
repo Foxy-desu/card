363d183b966876d76d24e81fa658b8a9d50e8c97
Alexandra Orliuk <iammeduza@gmail.com>
2024-07-16 12:21:14 +0300

    feat: add dockerfile for project containerization

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,15 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/card/',
+  base: '/',
+  preview: {
+    port: 8080,
+    strictPort: true,
+   },
+   server: {
+    port: 8080,
+    strictPort: true,
+    host: true,
+    origin: "http://127.0.0.1:8080",
+   },
 });
